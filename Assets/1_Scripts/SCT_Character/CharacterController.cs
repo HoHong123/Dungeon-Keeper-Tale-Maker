5aa27f1064228354f95aa8e3b06f6380be469ed6
dev-KH <epzmfhs@gmail.com>
2023-04-10 01:06:49 +0900

    feat : Add Character Controller

--- Assets/1_Scripts/SCT_Character/CharacterController.cs
+++ Assets/1_Scripts/SCT_Character/CharacterController.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// HP
+// Skill Delay
+// Strength
+// Defence
+// Agility
+// Fire rate
+// Speed
 public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
