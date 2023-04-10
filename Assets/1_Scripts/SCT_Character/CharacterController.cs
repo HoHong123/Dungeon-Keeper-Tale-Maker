cfc94b97e9b3436b7d98fd18f16692ed4f6339d4
dev-KH <epzmfhs@gmail.com>
2023-04-11 01:47:54 +0900

    feat : Add force test on character

--- Assets/1_Scripts/SCT_Character/CharacterController.cs
+++ Assets/1_Scripts/SCT_Character/CharacterController.cs
@@ -12,6 +12,10 @@ using UnityEngine;
 // Speed
 public class CharacterController : MonoBehaviour
 {
+    [SerializeField] private bool b_direction;
+    [SerializeField] private ForceMode e_mode;
+    [SerializeField] private Rigidbody rb_character;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,6 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb_character.AddForce(Vector3.right * (b_direction ? 1 : -1), e_mode);
     }
 }
